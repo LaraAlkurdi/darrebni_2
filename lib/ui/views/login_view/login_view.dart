306288a5ef8ae7ccd994a8fd17838e8e2cc56011
LaraAlkurdi <larahoms22@gmail.com>
2023-08-08 16:21:39 +0300

    o

--- lib/ui/views/login_view/login_view.dart
+++ lib/ui/views/login_view/login_view.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 import 'package:impty_project/ui/shared/colors.dart';
 import 'package:impty_project/ui/shared/custom_widgets/custom_button.dart';
 import 'package:impty_project/ui/shared/custom_widgets/custom_images.dart';
 import 'package:impty_project/ui/shared/custom_widgets/custom_text.dart';
 import 'package:impty_project/ui/shared/custom_widgets/custom_text_field.dart';
 import 'package:impty_project/ui/shared/utils.dart';
+import 'package:impty_project/ui/views/login_view/login_controller.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -15,82 +16,93 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  LoginController controller = Get.put(LoginController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Padding(
         padding: EdgeInsets.symmetric(
             vertical: screenWidth(10), horizontal: screenWidth(20)),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: CustomText(
-                  text: 'تسجيل الدخول',
-                  textColor: AppColors.mainTextColor,
-                  textsize: screenWidth(18)),
-            ),
-            CustomImages(imageName: 'Login'),
-            SizedBox(
-              height: screenHeight(30),
-            ),
-            CustomText(
-                text: 'اسم المستخدم',
-                textColor: AppColors.mainPurpleColor,
-                textsize: screenWidth(20)),
-            SizedBox(
-              height: screenHeight(90),
-            ),
-            CustomTextField(
-              hintext: 'اسم المستخدم',
-              prifexIcon: Icons.person_outline,
-            ),
-            SizedBox(
-              height: screenHeight(90),
-            ),
-            CustomText(
-                text: 'رمز تسجيل الدخول',
-                textColor: AppColors.mainPurpleColor,
-                textsize: screenWidth(20)),
-            SizedBox(
-              height: screenHeight(90),
-            ),
-            CustomTextField(
-              hintext: 'رمز تسجيل الدخول',
-              prifexIcon: Icons.key,
-            ),
-            SizedBox(
-              height: screenHeight(20),
-            ),
-            CustomButton(text: 'تسجيل الدخول'),
-            SizedBox(
-              height: screenHeight(80),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CustomText(
-                    text: 'ليس لديك حساب؟',
+        child: Form(
+          key: controller.formKey,
+          child: ListView(
+            children: [
+              Center(
+                child: CustomText(
+                    text: 'تسجيل الدخول',
+                    textColor: AppColors.mainTextColor,
+                    textsize: screenWidth(18)),
+              ),
+              CustomImages(imageName: 'Login'),
+              SizedBox(
+                height: screenHeight(30),
+              ),
+              CustomText(
+                  text: 'اسم المستخدم',
+                  textColor: AppColors.mainPurpleColor,
+                  textsize: screenWidth(20)),
+              SizedBox(
+                height: screenHeight(90),
+              ),
+              CustomTextField(
+                hintext: 'اسم المستخدم',
+                prifexIcon: Icons.person_outline,
+                controller: controller.userNameController,
+                validator: (value) {
+                  if (value!.isEmpty || !isName(value)) {
+                    return 'please check your email';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: screenHeight(90),
+              ),
+              CustomText(
+                  text: 'رمز تسجيل الدخول',
+                  textColor: AppColors.mainPurpleColor,
+                  textsize: screenWidth(20)),
+              SizedBox(
+                height: screenHeight(90),
+              ),
+              CustomTextField(
+                hintext: 'رمز تسجيل الدخول',
+                prifexIcon: Icons.key,
+              ),
+              SizedBox(
+                height: screenHeight(20),
+              ),
+              CustomButton(text: 'تسجيل الدخول'),
+              SizedBox(
+                height: screenHeight(80),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomText(
+                      text: 'ليس لديك حساب؟',
+                      textColor: AppColors.mainTextGreyColor,
+                      textsize: screenWidth(24)),
+                  CustomText(
+                      text: 'انشأ حسابك الآن',
+                      textColor: AppColors.mainPurpleColor2,
+                      textsize: screenWidth(24)),
+                ],
+              ),
+              SizedBox(
+                height: screenHeight(16),
+              ),
+              Center(
+                child: CustomText(
+                    onPressed: () {},
+                    textDecoration: TextDecoration.underline,
+                    text: 'المتابعة كزائر',
                     textColor: AppColors.mainTextGreyColor,
                     textsize: screenWidth(24)),
-                CustomText(
-                    text: 'انشأ حسابك الآن',
-                    textColor: AppColors.mainPurpleColor2,
-                    textsize: screenWidth(24)),
-              ],
-            ),
-            SizedBox(
-              height: screenHeight(16),
-            ),
-            Center(
-              child: CustomText(
-                  onPressed: () {},
-                  textDecoration: TextDecoration.underline,
-                  text: 'المتابعة كزائر',
-                  textColor: AppColors.mainTextGreyColor,
-                  textsize: screenWidth(24)),
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
