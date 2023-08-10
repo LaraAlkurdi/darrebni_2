e31cf9435419ab0bfbda593439f3a892dc0cd842
LaraAlkurdi <larahoms22@gmail.com>
2023-08-10 16:50:39 +0300

    o

--- lib/ui/shared/custom_widgets/custom_images.dart
+++ lib/ui/shared/custom_widgets/custom_images.dart
@@ -3,14 +3,18 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:impty_project/ui/shared/utils.dart';
 
 class CustomImages extends StatelessWidget {
-  const CustomImages({super.key, required this.imageName});
+  const CustomImages(
+      {super.key, required this.imageName, this.imageSize, this.imageColor});
   final String imageName;
+  final double? imageSize;
+  final Color? imageColor;
   @override
   Widget build(BuildContext context) {
     return Center(
       child: SvgPicture.asset(
         'assets/images/$imageName.svg',
-        width: screenWidth(1.9),
+        width: imageSize ?? screenWidth(1.9),
+        color: imageColor,
       ),
     );
   }
