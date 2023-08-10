8f8bdb63dd1825110f1cbd6e4790446404769fa4
LaraAlkurdi <larahoms22@gmail.com>
2023-08-11 00:34:39 +0300

    mmk

--- lib/ui/views/splash_screen/splash_screencontroller.dart
+++ lib/ui/views/splash_screen/splash_screencontroller.dart
@@ -1,23 +1,12 @@
 import 'package:get/get.dart';
-import 'package:impty_project/core/utils/general_util.dart';
-<<<<<<< HEAD
 import 'package:impty_project/ui/views/login_view/login_view.dart';
-=======
-import 'package:impty_project/ui/views/main_view/home_view/home_view.dart';
-import 'package:impty_project/ui/views/main_view/main_view.dart';
->>>>>>> main
 
 class SplashController extends GetxController {
   @override
   void onInit() {
     Future.delayed(Duration(seconds: 3)).then((value) {
-<<<<<<< HEAD
       // if (storage.getFirstLunch()  )
       Get.off(LoginView());
-=======
-      // if (storage.getFirstLunch())
-      Get.off(MainView());
->>>>>>> main
       // else {
       //  Get.off(storage.isLoggedIn? MainView():LandingView());
       // }
